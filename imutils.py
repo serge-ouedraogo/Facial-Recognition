5858046517ec48fa83d062e02c85c75e2f6338a1
Serge Ouedraogo <sergeoued3@gmail.com>
2019-06-12 19:06:26 -0700

    Facial Recognition

---
+++ imutils.py
@@ -0,0 +1,103 @@
+import glob
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import matplotlib.image as mpimg
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+    
+# tranforms
+
+class Norm(object):
+    """Convert a color image to grayscale and normalize the color range to [0,1]."""        
+
+    def __call__(self, image):
+        
+        image_copy = np.copy(image)
+        # convert image to grayscale
+        image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        
+        # scale color range from [0, 255] to [0, 1]
+        image_copy=  image_copy/255.0
+            
+ 
+        return image_copy 
+
+
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, image):
+        
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = cv2.resize(image, (new_w, new_h))
+
+        return img
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, image):
+        
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h,
+                      left: left + new_w]
+        return image
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, image):
+        # if image has no grayscale color channel, add one
+        if(len(image.shape) == 2):
+            # add that third color dim
+            image = image.reshape(image.shape[0], image.shape[1], 1)
+            
+            
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        
+        return  torch.from_numpy(image)
